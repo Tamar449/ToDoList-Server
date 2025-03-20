f2f390be1f55bac7d7f1717860b0ccbae2b0a1ae
Tamar Biton <tamar.b254@gmail.com>
2025-03-20 19:09:26 +0200

    add project to git

---
+++ Program.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ToDoDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("ToDoDB"))));
+
+
+builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        }));
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("MyPolicy");
+
+app.MapGet("/", async (ToDoDbContext Db) =>
+{
+    return await Db.Items.ToListAsync();
+});
+
+app.MapPost("/", async (String name, ToDoDbContext Db) =>
+{
+    var todoItem = new Item
+    {
+        IsComplete = false,
+        Name = name
+    };
+
+    Db.Items.Add(todoItem);
+    await Db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todoItem.Id}", todoItem);
+});
+
+app.MapPut("/{id}", async (int Id, bool IsComplete, ToDoDbContext Db) =>
+{
+    var todo = await Db.Items.FindAsync(Id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.IsComplete = IsComplete;
+    await Db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/{id}", async (int Id, ToDoDbContext Db) =>
+{
+    if (await Db.Items.FindAsync(Id) is Item todo)
+    {
+        Db.Items.Remove(todo);
+        await Db.SaveChangesAsync();
+        return Results.Ok(todo);
+    }
+
+    return Results.NotFound();
+});
+
+
+app.Run();
+
+
+
+
