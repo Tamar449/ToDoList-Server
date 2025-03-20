647a01d65faef69133b25750cfcb19460ddb3e8a
Tamar Biton <tamar.b254@gmail.com>
2025-03-20 20:12:31 +0200

    remove if before swagger

--- Program.cs
+++ Program.cs
@@ -25,11 +25,9 @@ builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseCors("MyPolicy");
 
