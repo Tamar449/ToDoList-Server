b27b2910649cc2cc803d77f42108a352efc969c5
Tamar Biton <tamar.b254@gmail.com>
2025-03-24 15:02:01 +0200

    good

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ToDoDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"),
-    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("ToDoDB"))));
+    new MySqlServerVersion(new Version(8, 0, 23)))); // החלף לגרסה שמתאימה לך
 
 
 builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
